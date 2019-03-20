60362dff7a57b86523168e799637f42600e9014e
Lance Hamilton <lance.hamilton24@gmail.com>
2019-03-19 19:08:33 -0500

    setup

--- Candy.cs
+++ Candy.cs
@@ -2,6 +2,11 @@
 {
     internal class Candy
     {
+        public int Id { get; set; }
         public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public string Flavor { get; set; }
+        public string DateReceived { get; set; }
+        public bool IsEaten { get; set; }
     }
 }
