c8d1fe572e62d5842d9f3d88647ecb098a356184
unknown <c.chase.hamby@gmail.com>
2019-03-19 19:24:37 -0500

    added flavors enum

--- Candy.cs
+++ Candy.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Manufacturer { get; set; }
-        public string Flavor { get; set; }
+        public Flavors Flavor { get; set; }
         public string DateReceived { get; set; }
         public bool IsEaten { get; set; }
     }
---
+++ Flavors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace candy_market
+{
+    enum Flavors
+    {
+        Chocolate,
+        Gummy,
+        Sour,
+        Strawberry,
+        Nuts,
+        Fruity,
+        Wafer,
+        Vanilla,
+        Caramel
+    }
+}
